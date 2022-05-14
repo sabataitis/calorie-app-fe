de2d1769608f8ff175c743b14d364215e044606d
Vilius Sabataitis <sabataitis@Viliuss-MacBook-Air.local>
2022-05-14 18:53:28 +0300

    add meta tags, improve lighthouse metrics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from "./shared/services/auth.service";
 import {Store} from "@ngrx/store";
 import {StoreActions} from "./store";
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,14 @@ import {StoreActions} from "./store";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  constructor(private authService: AuthService, private store: Store) {
-  }
+  constructor(private meta: Meta, private authService: AuthService, private store: Store) {}
+  keywords = 'kalorijos, kalorijų skaičiuoklė, mityba, sveika gyvensena, calorie counter, calorie app';
+  description: string = 'Kalorijų skaičiuoklė'
+
   ngOnInit() {
+    this.meta.addTag({name: 'description', content: this.description});
+    this.meta.addTag({name: 'keywords', content: this.keywords});
+
     if(this.authService.isLoggedIn()){
       this.store.dispatch(StoreActions.getCurrentUser());
     }
