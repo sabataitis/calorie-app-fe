ce2761d4d20f9a2fc2445d97197bb573c358d71a
Vilius Sabataitis <sabataitis@Viliuss-MacBook-Air.local>
2022-05-14 19:18:21 +0300

    remove devtools module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
 import {StoreEffects} from "./store/effects";
 import {StoreReducer} from "./store/reducer";
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
@@ -28,7 +27,6 @@ import { NgChartsModule } from 'ng2-charts';
     BrowserAnimationsModule,
     StoreModule.forRoot({store: StoreReducer}),
     EffectsModule.forRoot([StoreEffects]),
-    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
     ToastrStoreModule,
     ToastrModule.forRoot(),
     NgChartsModule,
